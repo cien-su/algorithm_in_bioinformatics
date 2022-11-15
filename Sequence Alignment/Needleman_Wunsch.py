e05475d0842b39f675a637c8c1a8c33101d4e2c5
domain <99190994+cien-su@users.noreply.github.com>
2022-11-15 14:01:27 +0800

    Update Needleman_Wunsch.py
    Needleman_Wunsch.py v2.0

--- Sequence Alignment/Needleman_Wunsch.py
+++ Sequence Alignment/Needleman_Wunsch.py
@@ -1,144 +1,168 @@
-# %% 导入包
+# %% import pacakages
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-# %% 导入序列
+# %% import sequences using file path
 def file(path: str):
     """
-    导入文件中的序列
-    path: 文件路径
+    import sequences through file path
+    path: absolutely file path
     """
     with open(path) as file_obj:
         seq = file_obj.read()
-    # 返回序列
+    # return sequence
     return seq
 
-# %% 初始化
+# %% initializing
 def str_to_list(a: str, b: str):
     """
-    将序列字符串转换为单个字符列表
-    a: 序列一
-    b: 序列二
+    'ATTGC'→['A', 'T', 'T', 'G', 'C']
+    a: sequence 1
+    b: sequence 2
     """
     return list(a), list(b)
 
-def ini_matrix(l1: list, l2: list):
+def ini_matrix(l1: list, l2: list, gap):
     """
-    初始化罚分矩阵并计算边缘分值
-    l1: 序列一列表
-    l2: 序列二列表
+    initialize the score matrix
+    l1: list of sequence 1
+    l2: list of sequence 2
+    gap: insertion or deletion score
     """
-    # 获取序列长度构建初始矩阵
+    # construct the initial matrix
     n1 = len(l1)
     n2 = len(l2)
     score_matrix = np.zeros((n1+1, n2+1))
-    # 初始化边缘分
+    # initialize the edge scores
     for i in range(1, n1+1):
-        score_matrix[i][0] -= 3 * i
+        score_matrix[i][0] += gap * i
     for j in range(1, n2+1):
-        score_matrix[0][j] -= 3 * j
-    # 返回矩阵
+        score_matrix[0][j] += gap * j
+    # return matrix
     return score_matrix
 
-# %% 计分
-def score(matrix: np.array, l1: list, l2: list, match, mismatch, empty):
+# %% scoring
+def score(matrix: np.array, l1: list, l2: list, match, mismatch, gap):
     """
-    计算矩阵得分
-    matrix: 初始化的矩阵
-    l1: 序列一列表
-    l2: 序列二列表
-    match: 匹配得分
-    mismatch: 不匹配时得分
-    empty: 空位得分
+    compute the score matrix
+    matrix: initialized matrix
+    l1: list of sequence 1
+    l2: list of sequence 2
+    match: match score
+    mismatch: mismatch score
+    gap: insertion or deletion score
     """
-    # 循环计分
+    # scoring in loop
     for i in range(1, len(l1)+1):
         for j in range(1, len(l2)+1):
-            # 计算三类分值
-            from_left = matrix[i][j - 1] + empty  # 从左到右空位
-            from_above = matrix[i - 1][j] + empty  # 从上到下空位
-            if l1[i-1] == l2[j-1]:  # 对角线
-                from_diag = matrix[i - 1][j - 1] + match  # 匹配
+            # compute scores from three directions
+            from_left = matrix[i][j - 1] + gap  # from left
+            from_above = matrix[i - 1][j] + gap  # from above
+            if l1[i-1] == l2[j-1]:  # from diagonal line
+                from_diag = matrix[i - 1][j - 1] + match  # match
             else:
-                from_diag = matrix[i - 1][j - 1] + mismatch  # 不匹配
-            # 比较并赋分
+                from_diag = matrix[i - 1][j - 1] + mismatch  # mismatch
+            # compare and give the fina score
             matrix[i][j] = max(from_left, from_above, from_diag)
     return matrix
 
-# %% 回溯
-def trace_back(matrix: np.array, l1: list, l2: list):
+# %% trace back the path
+def trace_back(res: np.array, l1: list, l2: list, match, mismatch, gap):
     """
-    回溯获得匹配结果索引
-    matrix: 结果矩阵
-    l1: 序列一列表
-    l2: 序列二列表
+    trace back and get the index of alignment results
+    res: score matrix
+    l1: list of sequence 1
+    l2: list of sequence 2
+    match: match score
+    mismatch: mismatch score
+    gap: insertion or deletion score
     """
-    # 遍历回溯
-    i = len(l1)
-    j = len(l2)
-    match = [(i, j)]
-    while i > 1 and j > 1:
-        # 找到三个值中的最大值
-        max_val = max(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1])
-        if max_val == matrix[i - 1][j - 1]:
-            match.append((i - 1, j - 1))
-            i -= 1
-            j -= 1
-        elif max_val == matrix[i - 1][j]:
-            match.append((i - 1, j))
-            i -= 1
-        elif max_val == matrix[i][j - 1]:
-            match.append((i, j - 1))
-            j -= 1
-    if i == 1:  # 行到头，只往左取
-        k = j - 1
-        match += [(1, j) for j in range(k, 0, -1)]
-    elif j == 1:  # 列到头，只往上取
-        k = i - 1
-        match += [(i, 1) for i in range(k, 0, -1)]
-    # 逆序,返回坐标元组列表
-    match = match[::-1]
-    return match
+    path = []    # to store all path results
+    m_stack = [(len(l1), len(l2))]  # main stack
+    a_stack = []  # assistant stack
+    while m_stack:  # when main stack is not empty
+        # check if reach the final
+        if m_stack[-1] != (0, 0):
+            # If not, then check if main stack is longer than the other. If yes, update the assistant stack
+            if len(m_stack) > len(a_stack):
+                a_stack.append([])
+                row = m_stack[-1][0]
+                col = m_stack[-1][1]
+                if l1[row - 1] == l2[col - 1] and res[row][col] == res[row-1][col-1]+match:
+                    a_stack[-1].append((row-1, col-1))
+                elif res[row][col] == res[row-1][col-1]+mismatch:
+                    a_stack[-1].append((row-1, col-1))
+                if res[row][col] == res[row-1][col]+gap:
+                    a_stack[-1].append((row-1, col))
+                if res[row][col] == res[row][col-1]+gap:
+                    a_stack[-1].append((row, col-1))
+            # Check if the top element of assistant stack is empty list
+            elif a_stack[-1] != []:
+                m_stack.append(a_stack[-1].pop())
+            # The top element of assistant stack is empty list. Pop the top of main stack
+            elif a_stack[-1] == []:
+                a_stack.pop()
+                m_stack.pop()
+        else:
+            # if reach the final, then store the path and pop the top of main stack
+            path.append(m_stack.copy())
+            m_stack.pop()
+    return path
 
-# %% 将回溯结果转化为碱基
-def trace_to_base(match: list, l1: list, l2: list):
+# %% transfer index to name
+def trace_to_base(match: list, l1: list, l2: list): 
     """
-    将回溯获得的索引转化为对应的碱基并打印
-    match: 回溯索引元组列表
-    l1: 序列一列表
-    l2: 序列二列表
+    transfer index to corresponding name and print
+    match: list of index tuple
+    l1: list of sequence 1
+    l2: list of sequence 2
     """
-    seq_match1 = [l1[0]]
-    seq_match2 = [l2[0]]
+    seq_match1 = []
+    seq_match2 = []
+    connect = []  # connection tag
+    # loop transfer
     for index in range(1, len(match)):
         if match[index][0] != match[index - 1][0] and match[index][1] != match[index-1][1]:
             seq_match1.append(l1[match[index][0] - 1])
             seq_match2.append(l2[match[index][1] - 1])
+            if l1[match[index][0] - 1] == l2[match[index][1] - 1]:
+                connect.append("|")
+            else:
+                connect.append("*")
         elif match[index][0] == match[index - 1][0]:
             seq_match1.append("-")
             seq_match2.append(l2[match[index][1] - 1])
+            connect.append(" ")
         elif match[index][1] == match[index - 1][1]:
             seq_match1.append(l1[match[index][0] - 1])
             seq_match2.append("-")
+            connect.append(" ")
+    # transfer list to string
     seq_match1 = "".join(seq_match1)
     seq_match2 = "".join(seq_match2)
-    return seq_match1, seq_match2
+    connect = "".join(connect)
+    return seq_match1, seq_match2, connect
 
-# %% 可视化
-def matrix_visual(res_matrix: np.array, l1: list, l2: list, index: list):
+# %% visualizing
+def matrix_visual(res_matrix: np.array, l1: list, l2: list, index: list, plot_val=False):
     """
-    可视化得分矩阵
-    res_matrix: 得分矩阵
-    l1: 序列一列表
-    l2: 序列二列表
-    index: 回溯的索引列表
+    plot the score matrix
+    res_matrix: score matrix
+    l1: list of sequence 1
+    l2: list of sequence 2
+    index: list of index tuple
+    plot_val: Logical. Whether plot matrix value on the heatmap or not. The default is False
     """
-    # 可视化得分矩阵
+    # integrate all indexes
+    path = set()
+    for i in index:
+        path = set(i).union(path)
+    # score plot
     fig, ax = plt.subplots()
     visual_matrix = plt.imshow(res_matrix)
     plt.colorbar(visual_matrix)
-    # 修改轴
+    # set the axis
     l1.insert(0, '0')
     l2.insert(0, '0')
     ax.set_xticks(np.arange(0, len(l2)), minor=False)
@@ -149,58 +173,62 @@ def matrix_visual(res_matrix: np.array, l1: list, l2: list, index: list):
     ax.set_yticklabels(l1)
     plt.xlabel("Sequence 2")
     plt.ylabel("Sequence 1")
-    # 标记路径上的得分
-    for i in index:
-        ax.text(i[1] - 0.35, i[0] + 0.1, res_matrix[i[0]][i[1]], fontsize=6)
-    # 紧密排布
+    # check if plot_val is True
+    if plot_val:
+        for i in range(len(l2)):
+            for j in range(len(l1)):
+                if (j, i) in path:
+                    ax.text(i-0.35, j+0.1,
+                            res_matrix[j][i], fontsize=6, color="#FF49F5")
+                else:
+                    ax.text(i-0.35, j+0.1, res_matrix[j][i], fontsize=6)
+    # tightly layout
     plt.tight_layout()
     plt.show()
 
-# %% 主函数
-def nw(seq1: str, seq2: str, match=8, mismatch=-5, empty=-3, plot=False):
+# %% main function
+def nw(seq1: str, seq2: str, match, mismatch, gap, plot=False, plot_val=False):
     """
-    主函数
-    seq1: 第一条序列
-    seq2: 第二条序列
-    match: 匹配得分,默认为+8
-    mismatch: 不匹配时得分,默认为-5
-    empty: 空位得分,默认为-3
-    plot: 逻辑值,是否画出得分矩阵,默认不画出
+    main function
+    l1: list of sequence 1
+    l2: list of sequence 2
+    match: match score
+    mismatch: mismatch score
+    gap: insertion or deletion score
+    plot: Logical. Whether plot heatmap of score matrix or not. The default is False
+    plot_val: Logical. Whether plot matrix value on the heatmap or not. The default is False
     """
-    # 开始计时
-    print("\n--------------------")
-    print("开始匹配......")
+    # start time count
+    print("\nStart Alignment......")
     start = time.time()
-    # 获取列表
+    # Get the lists of two sequences
     l1, l2 = str_to_list(seq1, seq2)
-    # 初始化打分矩阵
-    score_matrix = ini_matrix(l1, l2)
-    # 为矩阵赋分
-    res_matrix = score(score_matrix, l1, l2, match, mismatch, empty)
-    # 回溯
-    match_index = trace_back(res_matrix, l1, l2)
-    seq_match1, seq_match2 = trace_to_base(match_index, l1, l2)
-    # 停止计时
+    # initialize score matrix
+    score_matrix = ini_matrix(l1, l2, gap)
+    # compute the score matrix
+    res_matrix= score(score_matrix, l1, l2, match, mismatch, gap)
+    # trace back the alignment
+    path = trace_back(res_matrix, l1, l2, match, mismatch, gap)
+    # stop time count
     stop = time.time()
-    print("匹配结束!")
-    # 打印最终结果
-    print("\n--------------------")
-    print("打印得分矩阵......")
-    print(res_matrix)
-    print("\n--------------------")
-    print("打印比对结果......")
-    print(seq_match1)
-    print(seq_match2)
-    print("\n--------------------")
-    print("打印计算耗时......")
-    print("耗时为: ", stop - start, "秒")
+    print("Finish!\n********************")
+    # print the final results
+    print("Print final results......")
+    for i in path:
+        i = i[::-1]
+        seq_match1, seq_match2, connect = trace_to_base(i, l1, l2)
+        print(seq_match1)
+        print(connect)
+        print(seq_match2)
+        print("--------------------")
+    print("%s results, scores: %s"%(len(path), res_matrix[-1][-1]))
+    print("\nTime consuming: ", stop - start, "seconds")
     if plot:
-        print("\n--------------------")
-        print("可视化得分矩阵......")
-        matrix_visual(res_matrix, l1, l2, match_index)
-        print("结束!")
+        print("\nPlot the score matrix......")
+        matrix_visual(res_matrix, l1, l2, path, plot_val)
+    print("\nOver!!!")
 
-# %% 应用
+# %% Application
 seq1 = "AACGTACTCAAGTCT"
 seq2 = "TCGTACTCTAACGAT"
-nw(seq1, seq2, plot=True)
+nw(seq1, seq2, match=9, mismatch=-6, gap=-2, plot=True, plot_val=True)
